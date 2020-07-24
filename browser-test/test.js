c16df79a2328d429a3195cfc96e64cf07a88d425
Carlos Filoteo <c.filoteo12@gmail.com>
2020-07-24 12:07:59 -0600

    Add tests

--- browser-test/test.js
+++ browser-test/test.js
@@ -1,9 +1,37 @@
-describe("@esm-bundle/autopublish-template", () => {
+describe("@esm-bundle/react-emotion", () => {
   it("can load the ESM bundle", () => {
-    return import("/base/esm/index.js");
+    return import("/base/esm/index.resolved.js");
   });
 
   it("can load the System.register bundle", () => {
     return System.import("/base/system/index.js");
+  });
+
+  it("System.register development bundle can be used", () => {
+    return System.import("/base/system/index.js").then((module) => {
+      const { default: styled } = module;
+      expect(styled).toBeDefined();
+      // Default export
+      expect(typeof styled).toBe("function");
+      // Works without error
+      const Button = styled("button")`
+        color: turquoise;
+      `;
+      expect(Button).toBeDefined();
+    });
+  });
+
+  it("System.register production bundle can be used", () => {
+    return System.import("/base/system/index.min.js").then((module) => {
+      const { default: styled } = module;
+      expect(styled).toBeDefined();
+      // Default export
+      expect(typeof styled).toBe("function");
+      // Works without error
+      const Button = styled("button")`
+        color: turquoise;
+      `;
+      expect(Button).toBeDefined();
+    });
   });
 });
